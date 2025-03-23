c8ca37fe834687f452a6b5a697705eb366932f16
HarshaVardhanReddy653 <kodiharshavardhanreddy1@gmail.com>
2025-03-23 12:00:24 +0530

    Create Dice_roll.py

---
+++ Dice_roll.py
@@ -0,0 +1,11 @@
+import random
+choice=input('Roll the Dice (yes/No)')
+if choice==yes:
+  die1=random.randint(1,6)
+  die2=random.randint(1,6)
+  print(f'{die1},{die2}')
+elif choice==No:
+  print(f'Thank u for playing')
+  break
+else:
+  print('innvalid choce!')
