6c358261188f5ac978c92c0dcd778d5693e5bd7d
HarshaVardhanReddy653 <kodiharshavardhanreddy1@gmail.com>
2025-03-23 12:11:10 +0530

    Update Dice_roll.py

--- Dice_roll.py
+++ Dice_roll.py
@@ -1,11 +1,11 @@
 import random
 while True:
 choice=input('Roll the Dice (yes/No)')
-if choice==yes:
+if choice=="yes":
   die1=random.randint(1,6)
   die2=random.randint(1,6)
   print(f'{die1},{die2}')
-elif choice==No:
+elif choice=="No":
   print(f'Thank u for playing')
   break
 else:
